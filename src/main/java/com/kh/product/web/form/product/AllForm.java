6cb88a71398e24b73b286ee1b50d767929eb049c
khstanley <stj9568@naver.com>
2023-10-17 15:58:25 +0900

    '231017test'

---
+++ src/main/java/com/kh/product/web/form/product/AllForm.java
@@ -0,0 +1,11 @@
+package com.kh.product.web.form.product;
+
+import lombok.Data;
+
+@Data
+public class AllForm {
+  private Long productId;
+  private String pname;
+  private Long quantity;
+  private Long price;
+}
